4df657c0841948991d05e8ebbed5ccaa7fca3d5e
lgezelius <lgezelius@gmail.com>
2014-06-18 07:26:41 -0700

    add an example to comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,17 @@
 ## A pair of functions that cache the inverse of a matrix.
 
+## Example:
+## > my_matrix = makeCacheMatrix(rbind(c(1, -1/4), c(-1/4, 1)))
+## > cacheSolve(my_matrix)
+## [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+## > cacheSolve(my_matrix)
+## getting cached data
+## [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+
 ## makeCacheMatrix function creates a special "matrix" object that can cache its inverse. 
 ## This functions creates a special "matrix", which is a list containing a function to
 ## - set the value of the matrix
